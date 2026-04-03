14fb8598d72648730ca50d72a189a9594f7a74ca
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-04-03 21:30:39 +0530

    Delete JSFoundation/part1 directory

--- JSFoundation/part1/printng.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello World");
-console.log("My name is Kartikey Singh");
-
-process.stdout.write("This is process.stdout.write output");
-process.stdout.write("This is process.stdout.write output");
-
-console.table({ city: "Ballia" });
-console.table("this is console.log");
-console.warn("this text is console.warn it changes colour when used in web browser.")

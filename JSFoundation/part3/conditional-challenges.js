39559e04594ddf95d70e2363769c9e0e0ee9edb5
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-04-05 00:07:14 +0530

    Add files via upload

---
+++ JSFoundation/part3/conditional-challenges.js
@@ -0,0 +1,59 @@
+// Challenge-1 Checking if a number is greater than another number
+
+let num1 = 5;
+let num2 = 8;
+
+/*
+if(num1>num2)
+    console.log(num1 + " is greater than "+ num2);
+else
+    console.log(num2 + " is greater")
+*/
+
+//Challenge-2 Checking if a string is equal to another string:
+
+let user1 = "Hannu";
+let user2 = "Gunnu";
+/*
+if(user1 == user2){
+    console.log("both user are same")
+}
+else
+    console.log("both are different")
+*/
+
+//Challenge-3 Checking if a variable is a number or not
+
+let score = 52;
+/*
+if (typeof score === "string") 
+    console.log("it is number");
+else 
+    console.log("It is not a number");
+*/
+
+//Challenge-4 Checking if a boolean value is true or false
+
+let isReady = false;
+/*
+if(isReady)
+    console.log("it is true")
+else
+    console.log("it is false");
+*/
+
+//Challenge-5 Checking if array is empty or not
+
+let a = [];
+/*
+if (a[0] == null){
+    console.log("array is empty");
+}
+else
+    console.log("array is not empty");
+*/
+
+/*
+if (a.length === 0) console.log("it is empty");
+else console.log("it is not empty");
+*/

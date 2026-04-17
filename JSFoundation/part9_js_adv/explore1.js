0f90aa098828cf7f5fc276438e1a447fe01b82e7
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-04-17 19:17:48 +0530

    Add files via upload

---
+++ JSFoundation/part9_js_adv/explore1.js
@@ -0,0 +1,16 @@
+console.log("chaicode");
+
+function hello(){
+    console.log("I would like to say hello");
+}
+
+setTimeout(()=>{
+    hello()
+}, 4000)
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
+
+
+

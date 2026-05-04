571f116ad40c7c665cb5a612be9468163a86922b
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-05-04 19:42:10 +0530

    Add files via upload

---
+++ NODE.JS/todo/todo.js
@@ -0,0 +1,54 @@
+const { log } = require("console");
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+const loadTasks = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const removeTask = (index) => {
+  const tasks = loadTasks();
+  if (index > 0 && index <= tasks.length) {
+    const removed = tasks.splice(index - 1, 1);
+    saveTasks(tasks);
+    console.log("Task removed:", removed[0].task);
+  } else {
+    console.log("Invalid index");
+  }
+};
+
+const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log("Task added", task);
+};
+
+const listTasks = () => {
+  const tasks = loadTasks();
+  tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
+};
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("Command not found");
+}

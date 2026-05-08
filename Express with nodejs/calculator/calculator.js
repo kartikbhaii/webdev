53357d2a8a8c3ef0bf647dba458e5a5629a93ef5
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-05-08 21:47:02 +0530

    Add npm body-parser and extract numbers from "HTML FORM" to calculate.
    Added npm install body-parser and extracted input from the "html form" and calculated and reflect in the browser. This way only html can be seen on client side, which they'll not abe to see any calculations as calculation is in this js (this is called backend).

--- Express with nodejs/calculator/calculator.js
+++ Express with nodejs/calculator/calculator.js
@@ -1,5 +1,9 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true})) // use to parse/extract/get data from 'HTML FORM' data.
+
 const port = 3000
 
 
@@ -7,8 +11,14 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
+app.post("/", (req,res)=>{
+    let num1 = Number(req.body.n1)
+    let num2 = Number(req.body.n2)
 
-
+    let result = num1 + num2
+    res.send(`The sum of the calculation is: ${result}`)
+    
+})
 
 app.listen(port, ()=>{
     console.log(`Server is on port ${port}`)

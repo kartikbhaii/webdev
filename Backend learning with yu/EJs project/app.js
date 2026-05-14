45d2465f901a2ea26ef4220a80b1727f053e2c0b
Kartikey Singh <92799229+kartikbhaii@users.noreply.github.com>
2026-05-14 21:05:06 +0530

    Implement work items and dynamic file serving
    Added functionality for work items and static files.

--- Backend learning with yu/EJs project/app.js
+++ Backend learning with yu/EJs project/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-let items = ["Buy food", "Cook Food", "Eat Food"]
+let items = []
+let workItems = [];
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true})) // must use to get text from 'html forms' as post request.
+ 
+app.use(express.static("public")) // this includes every file from the public folder for eg. css file or any js file, and includes it to our server.
 
 
 app.get("/", (req, res) => {
@@ -16,14 +20,40 @@ app.get("/", (req, res) => {
   };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { 
+    listTitle: day, 
+    newListItems: items 
+  });
 });
 
 app.post("/", (req, res) => {
-  item = req.body.newItem;
-  items.push(item)
-  res.redirect("/")
+  let item = req.body.newItem;
+  if (item === "Work") {
+    workItems.push(item)
+    res.redirect("/work")
+  } else{
+    items.push(item)
+    res.redirect("/")
+  }
+
 });
+
+app.get("/work", (req,res)=>{
+  res.render("list", {
+    listTitle: "Work List",
+     newListItems: workItems
+    })
+})
+
+app.post("/work", (req,res)=>{
+  let item = req.body.newItem
+  workItems.push(item)
+  res.redirect("/work")
+})
+
+app.get("/about", (req,res)=>{
+  res.render("about")
+})
 
 
 app.listen(3000, () => {
